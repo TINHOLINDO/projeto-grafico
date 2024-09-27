20d6fdf8b90adfe942b2d974f5244d007eb8a761
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-09-27 07:43:54 -0300

    Add files via upload

--- grafico/dadosGerais.js
+++ grafico/dadosGerais.js
@@ -10,7 +10,7 @@ async function vizualizarInformacoes() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('caixa-grafico__texto')
-    paragrafo.innerHTML = `Em busca de descobrir os jogos mais vendidos em 2023, foi feita uma série de pesquisas em diferentes fontes. Com o auxilio da IA do google, foi possível estimar que o jogo mais vendido foi ${celularMaisVendido} com um total de aproximadamente ${modelo} nas plataformas ${modelo}`
+    paragrafo.innerHTML = `Em busca de descobrir os jogos mais vendidos em 2023, foi feita uma série de pesquisas em diferentes fontes. Com o auxilio da IA do google, foi possível estimar que o jogo mais vendido foi <span>${celularMaisVendido}</span> com um total de aproximadamente <span>${modelo}</span> nas plataformas <span>${modelo}</span>`
 
     const caixa = document.getElementById('caixa-grafico')
     caixa.appendChild(paragrafo)
