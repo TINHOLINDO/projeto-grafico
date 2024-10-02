9c262c9748f4403c4f4c5180ae44ba538a3dd7e2
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-10-02 10:38:56 -0300

    Add files via upload

---
+++ grafico/graficoBarra.js
@@ -0,0 +1,25 @@
+
+async function criaGraficoBarra(){
+    const url = "https://raw.githubusercontent.com/TINHOLINDO/json-grafico-barras/refs/heads/main/tinho%20json"
+    const res = await fetch(url)
+    const dados = await res.json()
+    const modelo = Object.keys(dados)
+    const vendas = Object.values(dados)
+
+    const data = [
+        {
+        x: modelo,
+        y: vendas,
+        type: 'bar'
+        }
+    ]
+
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('caixa-grafico').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+
+}
+
+criaGraficoBarra()
